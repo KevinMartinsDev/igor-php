c567437331c12fd0436cffbab63adbb37933dd4e
KevinMartinsDev <kevin.martins@me.com>
2026-08-12 11:59:36 +0200

    CLEANUP: Revert temporary local playground changes inside examples/demo-leak
    - Delete temporary Tracer/Span files.
    - Restore config/services.yaml and LeakDemoController.php back to original states.

--- examples/demo-leak/src/Service/FakeSpan.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Service;
-
-class FakeSpan implements Span {
-    public function setAttribute(string $key, string $value): void {}
-}
--- examples/demo-leak/src/Service/TracerInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Service;
-
-interface TracerInterface {
-    public function makeSpan(): Span;
-    public function getSharedManager(): FakeEntityManager;
-}
